598ae23cbab6fda55619a9225626c628096510ee
zinntikumugai <uesitananame55@gmail.com>
2019-11-06 08:09:09 +0900

    ボタンのカウントアップ

--- smallIr.ino
+++ smallIr.ino
@@ -39,38 +39,48 @@ void setup()
 int masterMode = 0;
 int subMode = 0;
 boolean isRun = false;
+int buttonCounters[BUTTON_LEN] = {0};
 
 void loop()
 {
     String printStr = "";
     int lcdPos = 0;
     boolean buttonStatuss[BUTTON_LEN];
-
+    // button check
     for (int i = 0; i < BUTTON_LEN; i++)
     {
         buttonStatuss[i] = buttons[i].read();
     }
+    delay(100);
 
     for (int i = 0; i < BUTTON_LEN; i++)
     {
+        boolean oldStatus = buttonStatuss[i];
+        buttonStatuss[i] = buttons[i].read();
+        if ((buttonStatuss[i]) == HIGH && oldStatus == LOW)
+            buttonCounters[i]++;
+    }
+
+    // button status
+    for (int i = 0; i < BUTTON_LEN; i++)
+    {
         printStr += buttons[i].toString() + " ";
+        printStr += buttonCounters[i];
+        printStr += " ";
     }
     lcdD.append(printStr);
     lcdD.append("\n");
 
-    float pH = dht.readHumidity();
-    float tp = dht.readTemperature();
+    // float pH = dht.readHumidity();
+    // float tp = dht.readTemperature();
 
-    printStr = "";
-    printStr += "T:";
-    printStr += String(tp, 1);
-    printStr += "C H:";
-    printStr += String(pH, 1);
-    printStr += "%";
-    
-    lcdD.clear();
-    lcdD.append(printStr);
+    // printStr = "";
+    // printStr += "T:";
+    // printStr += String(tp, 1);
+    // printStr += "C H:";
+    // printStr += String(pH, 1);
+    // printStr += "%";
+
+    // lcdD.append(printStr);
     lcdD.show(lcd);
-
-    delay(100);
 }
